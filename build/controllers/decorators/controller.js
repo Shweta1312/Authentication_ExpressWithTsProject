0281f6c271967df92395f0ff72240b024e7a1823
Shweta1312 <shawant11@gmail.com>
2020-11-10 00:48:38 +0530

    Refactored using enum

--- build/controllers/decorators/controller.js
+++ build/controllers/decorators/controller.js
@@ -3,14 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.controller = exports.router = void 0;
 require("reflect-metadata");
 var AppRouter_1 = require("../../AppRouter");
+var MetaDataKeys_1 = require("./MetaDataKeys");
 exports.router = AppRouter_1.AppRouter.getInstance();
 function controller(routePrefix) {
     return function (target) {
         for (var key in target.prototype) {
             var routeHandler = target.prototype[key];
-            var path = Reflect.getMetadata('path', target.prototype, key);
+            var path = Reflect.getMetadata(MetaDataKeys_1.MetaDataKeys.path, target.prototype, key);
+            var method = Reflect.getMetadata(MetaDataKeys_1.MetaDataKeys.method, target.prototype, key);
             if (path) {
-                exports.router.get("" + routePrefix + path, routeHandler);
+                exports.router[method]("" + routePrefix + path, routeHandler);
             }
         }
     };
--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
+import { Methods } from './Methods';
+import { MetaDataKeys } from './MetaDataKeys';
 
 export const router = AppRouter.getInstance();
 
@@ -7,10 +9,11 @@ export function controller(routePrefix: string) {
   return function (target: Function) {
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
-      const path = Reflect.getMetadata('path', target.prototype, key);
+      const path = Reflect.getMetadata(MetaDataKeys.path, target.prototype, key);
+      const method: Methods = Reflect.getMetadata(MetaDataKeys.method, target.prototype, key);
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
       }
     }
   };
